8d9ad1f50a64770d8f88c082ac692a8f7eb327ea
KION <kion1491@gmail.com>
2024-11-05 14:13:44 +0900

    Initial commit

---
+++ src/components/AppView.js
@@ -0,0 +1,21 @@
+export default {
+    template: `
+        <main>
+            <section>
+                <book-component></book-component>
+            </section>
+            <section>
+                <book-component></book-component>
+            </section>
+            <section>
+                <book-component></book-component>
+            </section>
+            <section>
+                <book-component></book-component>
+            </section>
+            <section>
+                <book-component></book-component>
+            </section>
+        </main>
+    `
+}
---
+++ src/components/AppView.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <section>
+            <book-component></book-component>
+        </section>
+        <section>
+            <book-component></book-component>
+        </section>
+        <section>
+            <book-component></book-component>
+        </section>
+        <section>
+            <book-component></book-component>
+        </section>
+        <section>
+            <book-component></book-component>
+        </section>
+    </main>
+</template>
+
+<script>
+    export default {}
+</script>
