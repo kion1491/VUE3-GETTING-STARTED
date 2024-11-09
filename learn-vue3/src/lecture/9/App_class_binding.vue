d826482b08a69bc9cd395d61a2746ff94f1a3a41
kion1491 <kion1491@gmail.com>
2024-11-09 14:03:33 +0900

    class binding, style binding
    class binding, style binding

---
+++ learn-vue3/src/lecture/9/App_class_binding.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="text" :class="[classObject, specialClass]">텍스트입니다.</div>
+    <button v-on:click="toggle">TOGGLE</button>
+    <button v-on:click="toggleDanger">TOGGLE Danger</button>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+export default {
+  setup() {
+    const isActive = ref(true);
+    const toggle = () => {
+      isActive.value = !isActive.value;
+    };
+    // const classObject = reactive({
+    //   active: true,
+    //   "text-danger": true,
+    // });
+
+    const classObject = computed(() => {
+      return {
+        active: true && true,
+        "text-danger": true && true,
+      };
+    });
+
+    const specialClass = ref("specialClass");
+
+    const hasError = ref(false);
+    const toggleDanger = () => {
+      hasError.value = !hasError.value;
+    };
+    return {
+      isActive,
+      toggle,
+      hasError,
+      toggleDanger,
+      classObject,
+      specialClass,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.text-danger {
+  color: red;
+}
+</style>
---
+++ learn-vue3/src/lecture/9/App_style_binding.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div :style="styleObject">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </div>
+    <button v-on:click="fontSize--">-</button>
+    <button v-on:click="fontSize++">+</button>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+export default {
+  setup() {
+    // const styleObject = reactive({
+    //   color: "red",
+    //   fontSize: "13px",
+    // });
+
+    const fontSize = ref(13);
+    const styleObject = computed(() => {
+      return {
+        color: "red",
+        fontSize: fontSize.value + "px",
+      };
+    });
+    return { styleObject, fontSize };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
