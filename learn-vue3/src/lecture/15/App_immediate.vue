05b275def9ad9e9431b12202bb50e9049b33047c
kion1491 <kion1491@gmail.com>
2024-11-17 17:00:18 +0900

    watch, watcheffect
    immediate,
    watch,
    watchEffect

---
+++ learn-vue3/src/lecture/15/App_immediate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <p>{{ reverseMessage }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+export default {
+  setup() {
+    const message = ref("Hello Vue3");
+    const reverseMessage = ref("");
+    const reverseFunction = (newValue) => {
+      reverseMessage.value = newValue.split("").reverse().join("");
+    };
+    watch(message, reverseFunction);
+    reverseFunction(message.value);
+    return { message, reverseMessage };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lecture/15/App_watcheffect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form @submit.prevent="saveFunc(title, contents)">
+      <input type="text" v-model.lazy="title" placeholder="Title" />
+      <textarea
+        v-model.lazy="contents"
+        placeholder="Contents"
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
+
+      <hr />
+
+      <button>SAVE</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref, watchEffect } from "vue";
+export default {
+  setup() {
+    const title = ref("");
+    const contents = ref("");
+    const saveFunc = (title, contents) => {
+      console.log("저장되었습니다. title: ", title, ", contents: ", contents);
+    };
+    watchEffect(() => {
+      console.log(title.value);
+      console.log(contents.value);
+      saveFunc(title.value, contents.value);
+    });
+
+    return { title, contents, saveFunc };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
